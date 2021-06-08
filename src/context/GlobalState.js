2df4ec086dd425b12f1124cef5c9d8b6ea6069b5
adiporn <kiraso74@gmail.com>
2021-06-08 15:40:04 +0700

    Add reducer and create context api

--- src/context/GlobalState.js
+++ src/context/GlobalState.js
@@ -1,5 +1,5 @@
 import React, { createContext, useReducer } from "react";
-import AppReducer from './AppReducer';
+import AppReducer from "./AppReducer";
 
 const initialState = {
   transactions: [
@@ -15,10 +15,31 @@ export const GlobalContext = createContext(initialState);
 
 //Provider component
 export const GlobalProvider = ({ children }) => {
-  const [state, diepatch] = useReducer(AppReducer, initialState);
+  const [state, dispatch] = useReducer(AppReducer, initialState);
+
+  //Actions
+  function deleteTransaction(id) {
+    dispatch({
+      type: "DELETE_TRANSACTION",
+      payload: id,
+    });
+  }
+
+  function addTransaction(transaction) {
+    dispatch({
+      type: "ADD_TRANSACTION",
+      payload: transaction,
+    });
+  }
 
   return (
-    <GlobalContext.Provider value={{ transactions: state.transactions }}>
+    <GlobalContext.Provider
+      value={{
+        transactions: state.transactions,
+        deleteTransaction,
+        addTransaction,
+      }}
+    >
       {children}
     </GlobalContext.Provider>
   );
